8aed0345e75a05548a3320b7904d1d4084c029db
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-09-24 17:05:16 +0200

    solve palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let str1 = str
+        .toLowerCase()
+        .split("")
+        .filter(val => val !== "." && val !== " " && val !== "!" && val !== ",")
+        .join("");
+    
+    let str2 = str
+        .toLowerCase()
+        .split("")
+        .filter(val => val !== "." && val !== " " && val !== "!" && val !== ",")
+        .reverse()
+        .join("");
 
+    if (str2 === str1) {
+        return true
+    } else {
+        return false
+    }
 };
+
+// const palindromes = function (str) {
+//     return str
+//         .toLowerCase()
+//         .split("")
+//         .filter(val => val !== "." && val !== " " && val !== "!" && val !== ",")
+//         .reverse()
+//         .join("");
+// };
+
+// console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
