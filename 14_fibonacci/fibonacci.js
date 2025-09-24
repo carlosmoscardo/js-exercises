a5c8e64a03a9e92c934820ed9b4239f292e6d0f4
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-09-24 17:55:02 +0200

    solve fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS"
+    } else if (num === 0 || num === "0") {
+        return 0
+    } else {
+        let fibo = [1, 1];
+        for (let i = 1; i < num - 1; i++) {
+            fibo.push((fibo[fibo.length - 1] + fibo[fibo.length - 2]));
+        }
+        return fibo[num - 1];
+    }
 };
+
+fibonacci(4); // returns the 4th member of the series: 3  (1, 1, 2, 3)
 
 // Do not edit below this line
 module.exports = fibonacci;
