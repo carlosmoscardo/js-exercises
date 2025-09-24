79c9d4ae3cb0a36d298dabced7f732b6c047311b
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-09-24 15:38:34 +0200

    sum tests

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -6,8 +6,12 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+	for (let i = 0; i < arr.length; i++) {
+		total += arr[i];
+	}
+	return total;
 };
 
 const multiply = function() {
