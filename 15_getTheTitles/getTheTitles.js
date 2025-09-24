be7ecb625a2ce2825547232ca2ae6ff7fb27d6ee
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-09-24 18:07:30 +0200

    solve object & array exercise

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+];
 
+const getTheTitles = function(arr) {
+    return arr.reduce((acc, val) => {
+        acc.push(val.title);
+        return acc;
+    }, []);
 };
+
+getTheTitles(books) // ['Book','Book2']
 
 // Do not edit below this line
 module.exports = getTheTitles;
