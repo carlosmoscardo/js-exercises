3071d92be9b72010c2565736c36a0cdf0dfe2214
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-09-25 20:58:17 +0200

    Solve array exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...remove) {
+    for (let i = 0; i < arr.length; i++) {  
+        if (remove.includes(arr[i])) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return arr;
+}
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
+
+console.log(removeFromArray([1, 2, 3, 4], 3)); //should remove 3 and return [1,2,4]
 
 // Do not edit below this line
 module.exports = removeFromArray;
