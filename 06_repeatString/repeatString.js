c1d5d8ef014cc9ec2b68980657d29be2ba55454e
Carlos <216593078+carlosmoscardo@users.noreply.github.com>
2025-09-25 20:17:45 +0200

    Solve array exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let sol = "";
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < num; i++) {
+        sol += str;
+    }
 
-};
+    }
+    return sol;
+}
+
+console.log(repeatString("hey", -1));
 
 // Do not edit below this line
 module.exports = repeatString;
